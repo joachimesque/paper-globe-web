8f83ef0c05b34f6fdbe72fd644e6ad189a8728a9
Joachim <joachim.robert@protonmail.com>
2022-08-07 14:02:22 +0200

    Add Form verification for link mime-type 🩺

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,21 @@
 """Forms"""
 
+import importlib
+import os
+
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
+import requests
 from wtforms import URLField, RadioField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, ValidationError
 
 from paperglobe import PROJECTIONS, PRINT_SIZES
+
+
+class_object = os.environ["APP_SETTINGS"]
+module_name, class_name = class_object.rsplit(".", maxsplit=1)
+config_module = importlib.import_module(module_name)
+mimetypes_allowed = getattr(config_module, class_name).MIMETYPES_ALLOWED
 
 image_type_choices = [
     ("preset", "select preset"),
@@ -22,9 +32,8 @@ print_format_choices = [(name, name) for item, name in PRINT_SIZES.items()]
 projection_choices = [(name, name) for item, name in PROJECTIONS.items()]
 
 
-# pylint: disable:too-few-public-methods
-class RequiredIf(DataRequired):
-    """Validator which makes a field required if another field is set and has a truthy value.
+class RequiredIf(DataRequired):  # pylint: disable=R0903
+    """Validator that makes a field required if another field is set and has a truthy value.
     https://gist.github.com/devxoul/7638142?permalink_comment_id=2601001#gistcomment-2601001
 
     Sources:
@@ -35,7 +44,7 @@ class RequiredIf(DataRequired):
 
     field_flags = ("requiredif",)
 
-    def __init__(self, *args, message=None, **kwargs):
+    def __init__(self, message=None, **kwargs):
         super(RequiredIf).__init__()
         self.message = message
         self.conditions = kwargs
@@ -51,11 +60,33 @@ class RequiredIf(DataRequired):
             Optional()(form, field)
 
 
+class MimeTypeAllowed:  # pylint: disable=R0903
+    """Validator that checks the Content-Type header of a response for the given URL"""
+
+    field_flags = ("MimeTypeAllowed",)
+
+    def __init__(self, *args, message=None):
+        super(MimeTypeAllowed).__init__()
+        self.message = message
+        self.mimetypes_allowed = args[0]
+
+    def __call__(self, form, field):
+        response_head = requests.head(field.data)
+        if response_head.headers["Content-Type"] not in self.mimetypes_allowed:
+            raise ValidationError(self.message)
+
+
 class UploadForm(FlaskForm):
     """Image upload/link form"""
 
     image_url = URLField(
-        "Paste your image URL", validators=[RequiredIf(image_type="url")]
+        "Paste your image URL",
+        validators=[
+            MimeTypeAllowed(
+                mimetypes_allowed, message="The linked file is not an image"
+            ),
+            RequiredIf(image_type="url"),
+        ],
     )
     image_file = FileField(
         "Upload your image file",
